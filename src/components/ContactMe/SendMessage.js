e96ec55e81e26097035d72e9d5d6a5d3e3040a95
Gaurav Gadkari <gaurav.gadkari1908@gmail.com>
2024-01-03 22:53:33 +0530

    firebase Integration

--- src/components/ContactMe/SendMessage.js
+++ src/components/ContactMe/SendMessage.js
@@ -1,8 +1,70 @@
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push,} from "firebase/database"
 
 export const SendMessage = () =>{
+    const [name,setName] = useState('');
+    const [mail,setMail] = useState('')
+    const [mess,setMess] = useState('')
+    const [subject,setSubject] = useState('')
+
+    const handleNameChange = event => {
+        setName(event.target.value)
+    }
+
+    const handleMailChange = event => {
+        setMail(event.target.value)
+    }
+
+    const handleMessChange = event => {
+        setMess(event.target.value)
+    }
+
+    const handleSubjectChange = event => {
+        setSubject(event.target.value)
+    }
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyCsmZutwY19mRZs7P5EKMu-tFn0YE33G4E",
+        authDomain: "gaurav-gadkari.firebaseapp.com",
+        databaseURL: "https://gaurav-gadkari-default-rtdb.firebaseio.com",
+        projectId: "gaurav-gadkari",
+        storageBucket: "gaurav-gadkari.appspot.com",
+        messagingSenderId: "237899186936",
+        appId: "1:237899186936:web:00058ee29d0dcccce7f10d",
+        measurementId: "G-89YVG06022"
+      };
+
+    
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+
+    const handleClick = (event) =>{
+        event.preventDefault();
+        console.log(name,mail,subject,mess)
+        const message = {
+            name: name,
+            mail: mail,
+            subject: subject,
+            message: mess
+        }
+        const databaseRef = ref(database, 'messages');
+        push(databaseRef, message)
+          .then(() =>{
+            // form.reset();
+            alert('message send Succesfully');
+            setName('')
+            setMail('')
+            setSubject('')
+            setMess('')
+          })
+          .catch((error) =>{
+            alert(error);
+          })
+    }
     return(
         <Col style={{
             backgroundColor:"white",
@@ -16,22 +78,22 @@ export const SendMessage = () =>{
                     <hr/>
                 </Col>
             </Row>
-            <Form>
+            <Form onSubmit={handleClick}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="Text" placeholder="Your Name" />
+                <Form.Control type="Text" placeholder="Your Name" value={name} onChange={handleNameChange}/>
             </Form.Group>
             <br/>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Your Email" />
+                <Form.Control type="email" placeholder="Your Email" value={mail} onChange={handleMailChange}/>
             </Form.Group>
             <br/>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="text" placeholder="Subject" />
+                <Form.Control type="text" placeholder="Subject" value={subject} onChange={handleSubjectChange}/>
             </Form.Group>
             <br/>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control as="textarea" rows={4} /> 
+                <Form.Control as="textarea" rows={4} value={mess} onChange={handleMessChange}/> 
             </Form.Group>
 
             <Button variant="primary" type="submit">
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,48 @@
 import { Row } from "react-bootstrap"
 import { ProjectCard } from "./ProjectCard"
 import { ProjectHeader } from "./ProjectHeader"
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue,} from "firebase/database";
+import { useEffect, useState } from "react";
 
 export const Projects = () =>{
-    const projects = require('../../resources/projects.json')
+    const [projects,setProjects] = useState([])
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() =>{
+        const firebaseConfig = {
+            apiKey: "AIzaSyCsmZutwY19mRZs7P5EKMu-tFn0YE33G4E",
+            authDomain: "gaurav-gadkari.firebaseapp.com",
+            databaseURL: "https://gaurav-gadkari-default-rtdb.firebaseio.com",
+            projectId: "gaurav-gadkari",
+            storageBucket: "gaurav-gadkari.appspot.com",
+            messagingSenderId: "237899186936",
+            appId: "1:237899186936:web:00058ee29d0dcccce7f10d",
+            measurementId: "G-89YVG06022"
+          };
+    
+        
+        const app = initializeApp(firebaseConfig);
+        const database = getDatabase(app);
+        const databaseRef = ref(database, 'projects');
+
+        onValue(databaseRef, (projects) => {
+            const data = projects.val();
+            setProjects(data);
+            console.log(data);
+            console.log(projects)
+            setLoading(false)
+          });
+
+    },[])
+
+    
+
+    
+    // console.log(data);
+    // const projects = require('../../resources/projects.json')
+   
+    console.log(projects);
     return(
         <div id="Projects" style={{
             paddingTop:"8%",
@@ -11,6 +50,9 @@ export const Projects = () =>{
         }}
         >
             <ProjectHeader/>
+            {loading ? (
+            <p>Loading...</p>
+        ) : (
             <Row>
                 {projects.map(
                     project=>
@@ -24,6 +66,8 @@ export const Projects = () =>{
                         />
                     )}
             </Row>
+        )}
+            
         </div>
     )
 }
